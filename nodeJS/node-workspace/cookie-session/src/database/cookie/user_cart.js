53f42f5e378b3ba6e495c12d566ffc1c10c73afd
Sua90827 <kimsuasch@gmail.com>
2023-07-31 17:49:37 +0900

    Folder name updated

--- nodeJS/node-workspace/cookie-session/src/database/cookie/user_cart.js
+++
@@ -1,6 +0,0 @@
-let cart = [
-    {goods_id : 1, title : "장난감", price : 5000},
-    {goods_id : 2, title : "컴퓨터", price : 10000},
-    {goods_id : 3, title : "자동차", price : 200}
-]
-module.exports = cart;
--- nodeJS/node-workspace/day02/service/test_service04.js
+++
@@ -1,6 +0,0 @@
-const member = require("../db/memberDAO");
-const index = () => {
-    console.log("service index connecting");
-    return member;
-}
-module.exports = {index};
